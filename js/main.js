a30e9294d409e7893ba3164198d1249fd3caafe7
cch5ng <cch5ng@gmail.com>
2015-08-18 21:46:49 -0700

    ported over remaining logic to get search input value and access in js. also the required event and key event listeners

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 
 //create model
 // var Food = Backbone.Model.extend({
-  //name: '',
-  //calories: 0,  
-  //defaults: {
-    
-  //},
-  
+	//name: '',
+	//calories: 0,  
+	//defaults: {
+		
+	//},
+	
 //   initialize: function() {
 //     console.log('');
 //   }
@@ -20,16 +20,41 @@
 
 //create model view
 var FoodView = Backbone.View.extend({
-  el: $('body'),
-  //events: {
-    
-  //},
-  initialize: function() {
-    this.render();
-  },
-  render: function() {
-    $(this.el).append("<form><input type='text' id='search' size='70' onkeypress='return noEnter(event)'><i class='ion ion-search'></i></form>");
-  }
+	el: $('body'),
+	//events: {
+		
+	//},
+	initialize: function() {
+		this.render();
+
+		var queryTerm;
+
+		//enter key press handler
+		//if focus is on the input field and user presses enter (keyboard up event)
+		//then the search should be triggered
+		function enterKeyUp(event) {
+			var input = document.querySelector('input');
+
+			if (event.key === 'Enter' || event.keyCode === 13 && input === document.activeElement) {
+				searchIconEventListener();
+			}
+		}
+
+		function searchIconEventListener() {
+			queryTerm = document.querySelector('#search').value;
+			console.log(queryTerm);
+		}
+
+			//search icon click listener
+		var searchIcon = document.querySelector('.ion-search');
+		searchIcon.addEventListener('click', searchIconEventListener);
+
+		//enter key press listener
+		document.querySelector('form').onkeypress = enterKeyUp;
+	},
+	render: function() {
+		$(this.el).append("<form><input type='text' id='search' size='70' onkeypress='return noEnter(event)'><i class='ion ion-search'></i></form>");
+	}
 });
 
 
@@ -42,8 +67,8 @@ var FoodView = Backbone.View.extend({
 //     console.log('');    
 //   }  
 // });  
-  
+	
 var foodView = new FoodView();  
-  
+	
 })(jQuery);
 
