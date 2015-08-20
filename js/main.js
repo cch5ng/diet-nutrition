b3eac1d206f36173fcf77a3c2e78e56148033d49
cch5ng <cch5ng@gmail.com>
2015-08-20 12:54:43 -0700

    moved getFoodCalories() logic under searchQueryStr() to better integrate with existing skeleton

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var FoodView = Backbone.View.extend({
 	el: $('body'),
 
 	events: {
-		'click .ion-search': 'searchQueryTerm',
+		'click .ion-search': 'searchQueryStr',
 		'keypress #search': 'enterKeyPress' 
 	},
 
@@ -42,9 +42,35 @@ var FoodView = Backbone.View.extend({
 		$(this.el).append("<ul></ul>");
 	},
 
-	searchQueryTerm: function() {
-		queryTerm = document.querySelector('#search').value;
-		console.log(queryTerm);
+	//helper func
+	cleanQueryStr: function(queryStr) {
+	  var strAr = queryStr.split(' ');
+	  return strAr.join('%20');
+	},
+
+//TODO parse the resulting object, maybe all of this logic should be merged into searchQueryTerm
+	//given a query string, returns an object that would be used to populate the food models
+	//the queryStr param should already have been cleaned up where spaces are replaced with %20
+	searchQueryStr: function() {
+		var queryStr = document.querySelector('#search').value;
+		console.log(queryStr);
+		var queryStr1 = cleanQueryStr(queryStr);
+	  var url = 'https://api.nutritionix.com/v1_1/search/' + queryStr1 + '?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=6a998f38&appKey=349a62ce80525685d939c45cb5faf8ee';
+	  
+	  var jqxhr = $.ajax({
+	    url: url,
+	    dataType: 'json'
+	  }).
+	    done(function(results) {
+	      console.log('results: ' + results);
+	      console.log('results: ' + results.hits[0].fields.item_name + ' ' + results.hits[0].fields.nf_calories + ' calories ' + results.hits[0].fields.brand_name);
+	    }).
+	    error(function(err) {
+	      console.log('Houston we have a problem: ' + err);
+	    }).
+	    always(function() {
+	      console.log('done');
+	    });
 	},
 
 		//enter key press handler
@@ -54,41 +80,9 @@ var FoodView = Backbone.View.extend({
 		var input = document.querySelector('input');
 
 		if (event.key === 'Enter' || event.keyCode === 13 && input === document.activeElement) {
-			this.searchQueryTerm();
+			this.searchQueryStr();
 		}
 	},
-
-	//helper func
-	cleanQueryStr: function(queryStr) {
-	  var strAr = queryStr.split(' ');
-	  return strAr.join('%20');
-	},
-
-//TODO
-	//given a query string, returns an object that would be used to populate the food models
-	//the queryStr param should already have been cleaned up where spaces are replaced with %20
-	getFoodCalories: function(queryStr) {  
-		var queryStr1 = cleanQueryStr(queryStr);
-		  
-		  var url = 'https://api.nutritionix.com/v1_1/search/' + queryStr1 + '?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=6a998f38&appKey=349a62ce80525685d939c45cb5faf8ee';
-		  
-		  var jqxhr = $.ajax({
-		    url: url,
-		    dataType: 'json'
-		  }).
-		    done(function(results) {
-		      console.log('results: ' + results);
-		      console.log('results: ' + results.hits[0].fields.item_name + ' ' + results.hits[0].fields.nf_calories + ' calories ' + results.hits[0].fields.brand_name);
-		    }).
-		    error(function(err) {
-		      console.log('Houston we have a problem: ' + err);
-		    }).
-		    always(function() {
-		      console.log('done');
-		    });
-	}
-});
-
 
 //create main view
 // var FoodListView = Backbone.View.extend({
