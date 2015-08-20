7a5eff53e0dad839f7c13d370eae4f28f1745d80
cch5ng <cch5ng@gmail.com>
2015-08-20 15:47:03 -0700

    added content for nextOrder() and comparator() functions. fixed typo for missing ","

--- js/collections/foods.js
+++ js/collections/foods.js
@@ -8,17 +8,20 @@ var FoodList = Backbone.Collection.extend({
 
 	model: app.Food,
 
-	localStorage: new Backbone.LocalStorage('foods-backbone') //,
+	localStorage: new Backbone.LocalStorage('foods-backbone'),
 
 	//TODO need something like nextOrder, comparator
 	//track order in which items added to list (db guid is unordered chronologically)
 	//just unclear where the order prop is getting set
 	nextOrder: function() {
-
+		if (!this.length) {
+			return 1;
+		}
+		return this.last().get('order') + 1;
 	},
 
-	comparator: function() {
-
+	comparator: function(todo) {
+		return todo.get('order');
 	}
 });
 
