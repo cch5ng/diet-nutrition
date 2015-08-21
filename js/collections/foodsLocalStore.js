189c55b2df68d3cf8dc4b92986bf7eb474008e6b
cch5ng <cch5ng@gmail.com>
2015-08-20 23:49:09 -0700

    init file

---
+++ js/collections/foodsLocalStore.js
@@ -0,0 +1,32 @@
+// js/collections/foodsLocalStore.js
+
+var app = app || {};
+
+//food collection
+
+var FoodsLocalStore = Backbone.Collection.extend({
+
+	model: app.Food,
+
+	localStorage: new Backbone.LocalStorage('foods-backbone'),
+
+	all: function() {
+		return this;
+	},
+
+	//TODO need something like nextOrder, comparator
+	//track order in which items added to list (db guid is unordered chronologically)
+	//just unclear where the order prop is getting set
+	nextOrder: function() {
+		if (!this.length) {
+			return 1;
+		}
+		return this.last().get('order') + 1;
+	},
+
+	comparator: function(todo) {
+		return todo.get('order');
+	}
+});
+
+app.FoodsLocalStore = new FoodsLocalStore();
