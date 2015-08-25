4721f2ad62ed4dbe67b380d5f446ce377f20a84d
cch5ng <cch5ng@gmail.com>
2015-08-24 17:25:41 -0700

    fixed syntax where the route needs to be a string and corresponding function needs to be a string too. home placeholder may be unnecessary. calories() function added

--- js/routers/router.js
+++ js/routers/router.js
@@ -6,9 +6,19 @@ var app = app || {};
 
 var Workspace = Backbone.Router.extend({
 	routes: {
-		*other: defaultRoute,
-		calories: getFoodsLocalStore
-	}//,
+		'*other': 'defaultRoute',
+		'calories' : 'calories' //retrieves list of foods/calories from localStorage with calorie total
+	},
+
+	//had issue using *other for defaultRoute so this is temp placeholder
+	home: function() {
+		console.log('placeholder');
+	},
+
+	calories: function() {
+//TODO, implement CaloriesView and figure out it releates to existing food.js model
+		this.view = new app.CalorieView();
+	}
 
 });
 
